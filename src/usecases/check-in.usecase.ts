89522c1b3664f5b31c8b36a8d1b549c79dc0d633
Leonardo Barrocal <leohb1501@gmail.com>
2024-08-05 22:33:59 -0300

    feat: implement check in use case

---
+++ src/usecases/check-in.usecase.ts
@@ -0,0 +1,34 @@
+import { CheckIn } from "@prisma/client";
+import { InvalidCredentialsError } from "./errors/invalid-credentials.error";
+import { CheckInsRepository } from "@/domain/check-ins.repository";
+
+interface CheckInUseCaseRequest {
+  userId: string;
+  gymId: string;
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn;
+}
+
+export class CheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) { }
+
+  async execute({
+    gymId,
+    userId
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.create({
+      gym_id: gymId,
+      user_id: userId,
+    });
+
+    if (!checkIn) {
+      throw new InvalidCredentialsError();
+    }
+
+    return {
+      checkIn,
+    }
+  }
+}
