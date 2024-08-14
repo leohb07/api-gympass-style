5f78c6b0948a58589ceecb0053711fad26f7846b
Leonardo Barrocal <lbarrocal@rssolutions.com.br>
2024-08-13 23:23:29 -0300

    feat: implement user check ins history use case

---
+++ src/usecases/user-check-ins-history.usecase.ts
@@ -0,0 +1,28 @@
+import { CheckIn } from "@prisma/client";
+import { CheckInsRepository } from "@/domain/check-ins.repository";
+
+interface UserCheckInsHistoryUseCaseRequest {
+  userId: string;
+  page: number;
+}
+
+interface UserCheckInsHistoryUseCaseResponse {
+  checkIns: CheckIn[];
+}
+
+export class UserCheckInsHistoryUseCase {
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+  ) { }
+
+  async execute({
+    userId,
+    page,
+  }: UserCheckInsHistoryUseCaseRequest): Promise<UserCheckInsHistoryUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(userId, page);
+
+    return {
+      checkIns,
+    }
+  }
+}
