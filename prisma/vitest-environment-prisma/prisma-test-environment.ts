3978bfcfed55990bc2f1d627ea07aae81a1c88cc
Leonardo Barrocal <lbarrocal@rssolutions.com.br>
2024-08-24 13:17:16 -0300

    refactor: update method setup and teardown to run tests e2e

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,45 @@
+import 'dotenv/config'
+
+import { PrismaClient } from "@prisma/client";
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  const databaseURL = process.env.DATABASE_URL
+
+  if (!databaseURL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.');
+  }
+
+  const url = new URL(databaseURL);
+
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   transformMode: "ssr",
   async setup() {
-    console.log("Setup");
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+
+    execSync('yarn prisma migrate deploy');
 
     return {
       async teardown() {
-        console.log("Teaddown");
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        );
+
+        await prisma.$disconnect();
       },
     };
   },
