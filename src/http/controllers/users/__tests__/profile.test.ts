e3d3d41deae58bef6dcea89bf89b5dcc185dd0d6
Leonardo Barrocal <leohb1501@gmail.com>
2024-08-27 22:36:58 -0300

    feat: create function to authenticate to use others tests

--- src/http/controllers/users/__tests__/profile.test.ts
+++ src/http/controllers/users/__tests__/profile.test.ts
@@ -1,43 +1,29 @@
-import request from 'supertest'
-import { app } from '@/app'
-import { afterAll, beforeAll, describe, it, expect } from 'vitest'
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, it, expect } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/create-and-authenticate-user.util";
 
-describe('Profile (e2e)', () => {
+describe("Profile (e2e)", () => {
   beforeAll(async () => {
-    await app.ready()
-  })
+    await app.ready();
+  });
 
   afterAll(async () => {
-    await app.close()
-  })
+    await app.close();
+  });
 
-  it('should be able to profile', async () => {
-    await request(app.server)
-      .post('/users')
-      .send({
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: '123456',
-      })
-
-    const authResponse = await request(app.server)
-      .post('/sessions')
-      .send({
-        email: 'johndoe@example.com',
-        password: '123456',
-      })
-
-    const { token } = authResponse.body
+  it("should be able to profile", async () => {
+    const { token } = await createAndAuthenticateUser(app);
 
     const profileResponse = await request(app.server)
-      .get('/me')
-      .set('Authorization', `Bearer ${token}`)
+      .get("/me")
+      .set("Authorization", `Bearer ${token}`);
 
-    expect(profileResponse.statusCode).toEqual(200)
+    expect(profileResponse.statusCode).toEqual(200);
     expect(profileResponse.body.user).toEqual(
       expect.objectContaining({
-        email: 'johndoe@example.com',
-      })
-    )
-  })
-})
+        email: "johndoe@example.com",
+      }),
+    );
+  });
+});
